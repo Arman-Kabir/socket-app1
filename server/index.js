b67ebc405ee7ae40d8ef563d22c1abd5b10cb406
Arman-Kabir <armankabir8@gmail.com>
2024-03-02 10:24:21 +0600

    simple server created and html page added

--- server/index.js
+++ server/index.js
@@ -3,6 +3,9 @@ const app = express();
 const http = require("http");
 const expressServer = http.createServer(app);
 
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + "/index.html");
+});
 
 expressServer.listen(3000, () => {
     console.log(`server is listening on port 3000`);
