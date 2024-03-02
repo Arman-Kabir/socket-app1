9b0fd4859a480c693d0ad53cb8be5b73c342b90e
Arman-Kabir <armankabir8@gmail.com>
2024-03-02 10:39:23 +0600

    socket configured in client and server

--- server/index.js
+++ server/index.js
@@ -3,6 +3,12 @@ const app = express();
 const http = require("http");
 const expressServer = http.createServer(app);
 
+const { Server } = require('socket.io');
+const io = new Server(expressServer);
+
+
+
+
 app.get('/', (req, res) => {
     res.sendFile(__dirname + "/index.html");
 });
